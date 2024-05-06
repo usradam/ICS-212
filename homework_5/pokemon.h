9177db8f5be6fdd0d6252ed7715ada6a4c0064df
Adam G <147444558+usradam@users.noreply.github.com>
2024-05-06 11:37:18 -1000

    Add files via upload

---
+++ homework_5/pokemon.h
@@ -0,0 +1,24 @@
+/******************************************************************
+//  
+//  NAME:        Adam Graham
+//
+//  HOMEWORK:    5
+//
+//  CLASS:       ICS 212
+//
+//  INSTRUCTOR:  Ravi Narayan
+//
+//  DATE:        February 20, 2024
+//
+//  FILE:        pokemon.h
+//
+//  DESCRIPTION:
+//   Definition for struct pokemon.
+//
+****************************************************************/
+
+struct pokemon
+{
+    int                level;
+    char               name[25];
+};
